9b2b96e157fd9690c21c2ca1ae2c11f7abb7d7d7
Joe Garcia, CISSP <joe.garcia@cyberark.com>
2017-11-15 21:04:51 -0500

    adding Editorial HTML5 UP free template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
 
+# Create the mappings from URLs
 @app.route('/')
 @app.route('/index')
+
+# Function to show index page
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Joe'} # fake user
+    return render_template('index.html',
+                            title='Home',
+                            user=user)
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 #!flask/bin/python
 
+# Import app variable from our app package
 from app import app
 
+# Invokes the run method to start the server
+## Don't forget the app variable holds the Flask instance
 app.run(debug=True)
