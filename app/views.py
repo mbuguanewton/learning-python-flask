1e2529a052f8089be11e6505507cf1666452ee26
Joe Garcia, CISSP <joe.garcia@cyberark.com>
2017-11-16 10:31:09 -0500

    Changed DEV to remove host

--- app/views.py
+++ app/views.py
@@ -8,6 +8,18 @@ from app import app
 # Function to show index page
 def index():
     user = {'nickname': 'Joe'} # fake user
+    posts = [ # fake array of posts
+        {
+            'author':   {'nickname': 'C-Mo'},
+            'body':     'Beautiful day in Portland!'
+        },
+        {
+            'author':   {'nickname': 'Rob'},
+            'body':     'The Avengers movie was so cool!'
+        }
+    ]
+
     return render_template('index.html',
                             title='Home',
-                            user=user)
+                            user=user,
+                            posts=posts)
