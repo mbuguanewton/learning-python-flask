80b7c88477aa5d9371830e659d027ef8235427f5
Joe Garcia, CISSP <joe.garcia@cyberark.com>
2017-11-16 09:46:13 -0500

    Added PROD & DEV run commands

--- run.py
+++ run.py
@@ -5,4 +5,7 @@ from app import app
 
 # Invokes the run method to start the server
 ## Don't forget the app variable holds the Flask instance
-app.run(debug=True)
+### DEVELOPMENT (Internal-facing, Debug on)
+#app.run(debug=True, host='192.168.2.200')
+### PRODUCTION (External-facing, Debug off)
+app.run(debug=False, host='0.0.0.0')
